16887729c1fe16ae9ee47b1c04b9d66f406237da
Lucia Perez <lu.perez09@gmail.com>
2023-01-24 18:11:32 -0400

    Se agrega el proyecto

---
+++ IntroASP/Controllers/ApiBeerController.cs
@@ -0,0 +1,32 @@
+﻿using IntroASP.Models;
+using IntroASP.Models.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroASP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiBeerController : ControllerBase
+    {
+        private readonly PubContext _contex;
+
+        public ApiBeerController(PubContext context)
+        {
+            _contex = context;
+        }
+
+       // [HttpGet]
+        public async Task<List<BeerBrandViewModel>> Get()
+        
+            => await _contex.Beers.Include(b => b.Brand)
+                .Select(b=>new BeerBrandViewModel
+                {
+                    Name=b.Name,
+                    Brand=b.Brand.Name,
+                })
+                .ToListAsync();
+        
+    }
+}
---
+++ IntroASP/Models/PubContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroASP.Models;
+
+public partial class PubContext : DbContext
+{
+    public PubContext()
+    {
+    }
+
+    public PubContext(DbContextOptions<PubContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Beer> Beers { get; set; }
+
+    public virtual DbSet<Brand> Brands { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-BHM979E; Database=Pub; Trusted_Connection=True; TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Beer>(entity =>
+        {
+            entity.HasKey(e => e.Beer1);
+
+            entity.ToTable("Beer");
+
+            entity.Property(e => e.Beer1)
+                .ValueGeneratedNever()
+                .HasColumnName("Beer");
+            entity.Property(e => e.Name)
+                .HasMaxLength(10)
+                .IsFixedLength();
+
+            entity.HasOne(d => d.Brand).WithMany(p => p.Beers)
+                .HasForeignKey(d => d.BrandId)
+                .HasConstraintName("FK_Beer_Brand");
+        });
+
+        modelBuilder.Entity<Brand>(entity =>
+        {
+            entity.ToTable("Brand");
+
+            entity.Property(e => e.BrandId)
+                .ValueGeneratedNever()
+                .HasColumnName("BrandID");
+            entity.Property(e => e.Name)
+                .HasMaxLength(10)
+                .IsFixedLength();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
